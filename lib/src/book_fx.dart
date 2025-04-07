b15ab726ce3baf5182d8d7eba2ca53fd2e3ac819
Cal Vidler <callum.vidler@hotmail.com>
2025-04-07 12:03:29 +0800

    migration commit

--- lib/src/book_fx.dart
+++ lib/src/book_fx.dart
@@ -129,7 +129,7 @@ class _BookFxState extends State<BookFx> with SingleTickerProviderStateMixin {
             widget.controller.currentIndex++;
             widget.nextCallBack?.call(widget.controller.currentIndex + 1);
           });
-        }
+        } else {}
         widget.onPageChanging?.call(false);
       }
       if (status == AnimationStatus.dismissed) {
@@ -248,6 +248,9 @@ class _BookFxState extends State<BookFx> with SingleTickerProviderStateMixin {
   }
 
   void gestureOnPanDown(DragStartDetails d) {
+    if (isAnimation) {
+      return;
+    }
     downPos = d.localPosition;
     // stopping tap down to cause page turn if previously occured, when no update occurs
     isNext = false;
@@ -324,8 +327,8 @@ class _BookFxState extends State<BookFx> with SingleTickerProviderStateMixin {
     }
     setState(() {
       isAlPath = false;
+      isAnimation = true;
     });
-    isAnimation = true;
     _controller?.forward(
       from: 0,
     );
@@ -356,9 +359,9 @@ class _BookFxState extends State<BookFx> with SingleTickerProviderStateMixin {
       height: widget.screenSize.height,
       child: GestureDetector(
         // can only be bigger than the size
-        onPanStart: gestureOnPanDown,
-        onPanUpdate: gestureOnPanUpdate,
-        onPanEnd: gestureOnPanEnd,
+        onHorizontalDragStart: gestureOnPanDown,
+        onHorizontalDragUpdate: gestureOnPanUpdate,
+        onHorizontalDragEnd: gestureOnPanEnd,
         behavior: HitTestBehavior.translucent,
         child: Align(
           alignment: Alignment.topLeft,
@@ -414,8 +417,9 @@ class _BookFxState extends State<BookFx> with SingleTickerProviderStateMixin {
     // controller called next page!!!
     setState(() {
       isAlPath = false;
+      isAnimation = true;
     });
-    isAnimation = true;
+
     isNext = true;
     isPrevious = false;
     currentA = Point(size.width - 50, size.height - 50);
@@ -485,204 +489,209 @@ class _BookPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.clipRect(Offset.zero & size);
-    Paint paint = Paint()
-      ..color = Colors.red
-      ..style = PaintingStyle.fill
-      ..strokeWidth = 2;
-    canvas.save();
-    canvas.translate(size.width / 2, size.height / 2);
+    try {
+      canvas.clipRect(Offset.zero & size);
+      Paint paint = Paint()
+        ..color = Colors.red
+        ..style = PaintingStyle.fill
+        ..strokeWidth = 2;
+      canvas.save();
+      canvas.translate(size.width / 2, size.height / 2);
 
-    canvas.restore();
+      canvas.restore();
 
-    Path mPathAB = Path();
+      Path mPathAB = Path();
 
-    ///书籍区域
-    Path mPath = Path();
-    mPath.addRect(Rect.fromCenter(
-        center: Offset(size.width / 2, size.height / 2),
-        width: size.width,
-        height: size.height));
+      ///书籍区域
+      Path mPath = Path();
+      mPath.addRect(Rect.fromCenter(
+          center: Offset(size.width / 2, size.height / 2),
+          width: size.width,
+          height: size.height));
 
-    /// A区域 下一页可见区域+当前页不可见区域  af重合 不需要绘制A区域
-    if (p.value.a != p.value.f) {
-      if (p.value.a.y == p.value.f.y) {
-        // canvas.drawPath(mPath, paint..color = Colors.yellow);
-        /// 翻页完毕
-      } else {
-        mPathAB.moveTo(p.value.c.x, p.value.c.y);
-        mPathAB.quadraticBezierTo(
-            p.value.e.x, p.value.e.y, p.value.b.x, p.value.b.y);
-        mPathAB.lineTo(p.value.a.x, p.value.a.y);
-        mPathAB.lineTo(p.value.k.x, p.value.k.y);
-        mPathAB.quadraticBezierTo(
-            p.value.h.x, p.value.h.y, p.value.j.x, p.value.j.y);
-        mPathAB.lineTo(p.value.f.x, p.value.f.y);
+      /// A区域 下一页可见区域+当前页不可见区域  af重合 不需要绘制A区域
+      if (p.value.a != p.value.f) {
+        if (p.value.a.y == p.value.f.y) {
+          // canvas.drawPath(mPath, paint..color = Colors.yellow);
+          /// 翻页完毕
+        } else {
+          mPathAB.moveTo(p.value.c.x, p.value.c.y);
+          mPathAB.quadraticBezierTo(
+              p.value.e.x, p.value.e.y, p.value.b.x, p.value.b.y);
+          mPathAB.lineTo(p.value.a.x, p.value.a.y);
+          mPathAB.lineTo(p.value.k.x, p.value.k.y);
+          mPathAB.quadraticBezierTo(
+              p.value.h.x, p.value.h.y, p.value.j.x, p.value.j.y);
+          mPathAB.lineTo(p.value.f.x, p.value.f.y);
 
-        mPathAB.close();
-        Path mPath1 = Path();
-        mPath1.moveTo(p.value.d.x, p.value.d.y);
-        mPath1.lineTo(p.value.a.x, p.value.a.y);
-        mPath1.lineTo(p.value.i.x, p.value.i.y);
-        mPath1.close();
+          mPathAB.close();
+          Path mPath1 = Path();
+          mPath1.moveTo(p.value.d.x, p.value.d.y);
+          mPath1.lineTo(p.value.a.x, p.value.a.y);
+          mPath1.lineTo(p.value.i.x, p.value.i.y);
+          mPath1.close();
 
-        /// C区域 当前页可见区域 与A区域重新生成新路径
-        // Path mPathC =
-        //     Path.combine(PathOperation.reverseDifference, mPathAB, mPath);
+          /// C区域 当前页可见区域 与A区域重新生成新路径
+          // Path mPathC =
+          //     Path.combine(PathOperation.reverseDifference, mPathAB, mPath);
 
-        // canvas.drawPath(mPathC, paint..color = Colors.red);
-        // canvas.drawPath(mPathAB, paint..color = Colors.yellow);
+          // canvas.drawPath(mPathC, paint..color = Colors.red);
+          // canvas.drawPath(mPathAB, paint..color = Colors.yellow);
 
-        /// B区域 当前页不可见区域
-        if (!p.value.b.x.isNaN) {
-          Path pyy1 = Path();
-          Path pyy2 = Path();
-          Path mPathB = Path.combine(PathOperation.intersect, mPathAB, mPath1);
-          // 已知a坐标点在一条直线上，求距离该坐标点为10并和该直线相交的的坐标点。
-          double m1 = (p.value.a.x - p.value.p.x);
-          double n1 = (p.value.a.y - p.value.p.y);
+          /// B区域 当前页不可见区域
+          if (!p.value.b.x.isNaN) {
+            Path pyy1 = Path();
+            Path pyy2 = Path();
+            Path mPathB =
+                Path.combine(PathOperation.intersect, mPathAB, mPath1);
+            // 已知a坐标点在一条直线上，求距离该坐标点为10并和该直线相交的的坐标点。
+            double m1 = (p.value.a.x - p.value.p.x);
+            double n1 = (p.value.a.y - p.value.p.y);
 
-          pyy1.moveTo(p.value.c.x - m1, p.value.c.y);
-          pyy1.quadraticBezierTo(p.value.e.x - m1, p.value.e.y - n1,
-              p.value.b.x - m1, p.value.b.y - n1);
+            pyy1.moveTo(p.value.c.x - m1, p.value.c.y);
+            pyy1.quadraticBezierTo(p.value.e.x - m1, p.value.e.y - n1,
+                p.value.b.x - m1, p.value.b.y - n1);
 
-          pyy1.lineTo(p.value.p.x, p.value.p.y);
-          pyy1.lineTo(p.value.k.x, p.value.k.y);
-          pyy1.lineTo(p.value.f.x, p.value.f.y);
-          pyy1.close();
+            pyy1.lineTo(p.value.p.x, p.value.p.y);
+            pyy1.lineTo(p.value.k.x, p.value.k.y);
+            pyy1.lineTo(p.value.f.x, p.value.f.y);
+            pyy1.close();
 
-          double mE1 = (p.value.a.x - p.value.p2.x);
-          double nE1 = (p.value.a.y - p.value.p2.y); // 负数
+            double mE1 = (p.value.a.x - p.value.p2.x);
+            double nE1 = (p.value.a.y - p.value.p2.y); // 负数
 
-          var twoPoint = PaperPoint.toTwoPoint(
-              Point(p.value.b.x - m1, p.value.b.y - n1),
-              p.value.p,
-              p.value.p2,
-              Point(p.value.k.x - mE1, p.value.k.y - nE1));
-          pyy2.moveTo(p.value.j.x, (p.value.j.y - nE1));
-          pyy2.quadraticBezierTo(p.value.i.x - mE1, p.value.i.y - nE1,
-              p.value.k.x - mE1, p.value.k.y - nE1);
+            var twoPoint = PaperPoint.toTwoPoint(
+                Point(p.value.b.x - m1, p.value.b.y - n1),
+                p.value.p,
+                p.value.p2,
+                Point(p.value.k.x - mE1, p.value.k.y - nE1));
+            pyy2.moveTo(p.value.j.x, (p.value.j.y - nE1));
+            pyy2.quadraticBezierTo(p.value.i.x - mE1, p.value.i.y - nE1,
+                p.value.k.x - mE1, p.value.k.y - nE1);
 
-          pyy2.lineTo(p.value.p2.x, p.value.p2.y);
-          pyy2.lineTo(p.value.b.x, p.value.b.y);
-          pyy2.lineTo(p.value.f.x, p.value.f.y);
+            pyy2.lineTo(p.value.p2.x, p.value.p2.y);
+            pyy2.lineTo(p.value.b.x, p.value.b.y);
+            pyy2.lineTo(p.value.f.x, p.value.f.y);
 
-          pyy2.close();
+            pyy2.close();
 
-          Path pd = Path();
-          pd.moveTo(p.value.a.x, p.value.a.y);
-          pd.lineTo(twoPoint.x, twoPoint.y);
-          pd.lineTo(p.value.p2.x, p.value.p2.y);
-          pd.close();
-          Paint pdPaint = Paint();
-          canvas.drawPath(
-              pd,
-              pdPaint
-                ..shader = ui.Gradient.linear(
-                  Offset(p.value.a.x, p.value.a.y),
-                  Offset(p.value.p2.x, p.value.p2.y),
-                  [Colors.black12, Colors.transparent],
-                )
-                ..style = PaintingStyle.fill);
-          Paint psPaint = Paint();
-          Path ps = Path();
-          ps.moveTo(p.value.a.x, p.value.a.y);
-          ps.lineTo(twoPoint.x, twoPoint.y);
-          ps.lineTo(p.value.p.x, p.value.p.y);
-          ps.close();
-
-          canvas.drawPath(
-              ps,
-              psPaint
-                ..shader = ui.Gradient.linear(
-                    Offset(p.value.a.x, p.value.a.y),
-                    Offset(p.value.p.x, p.value.p.y),
-                    [Colors.black12, Colors.transparent])
-                ..style = PaintingStyle.fill);
-          // canvas.drawPath(
-          //     Path.combine(PathOperation.reverseDifference, mPathAB, pyy2),
-          //     paint
-          //       ..color = Colors.green
-          //       ..style = PaintingStyle.stroke);
-          // //
-          // canvas.drawPath(
-          //     Path.combine(PathOperation.reverseDifference, mPathAB, pyy1),
-          //     paint
-          //       ..color = Colors.red
-          //       ..style = PaintingStyle.stroke);
-
-          // canvas.drawPath(
-          //     pyy2,
-          //     paint
-          //       ..color = Colors.red
-          //       ..style = PaintingStyle.fill);
-          // canvas.drawPath(
-          //     mPathAB,
-          //     paint
-          //       ..color = Colors.black
-          //       ..style = PaintingStyle.stroke);
-
-          Path startYY =
-              Path.combine(PathOperation.reverseDifference, mPathAB, pyy1);
-          Path endYY =
-              Path.combine(PathOperation.reverseDifference, mPathAB, pyy2);
-
-          canvas.drawPath(
-              mPathB, paint..color = bColor ?? Colors.grey.shade400);
-
-          Paint paint2 = Paint();
-          // 上左
-
-          canvas.drawPath(
-              startYY,
-              paint2
-                ..style = PaintingStyle.fill
-                ..shader = ui.Gradient.linear(
-                    Offset(p.value.a.x, p.value.a.y),
-                    Offset(p.value.p.x, p.value.p.y),
-                    [Colors.black12, Colors.transparent]));
-
-          //上右
-          canvas.drawPath(
-              endYY,
-              paint2
-                ..style = PaintingStyle.fill
-                ..shader = ui.Gradient.linear(
+            Path pd = Path();
+            pd.moveTo(p.value.a.x, p.value.a.y);
+            pd.lineTo(twoPoint.x, twoPoint.y);
+            pd.lineTo(p.value.p2.x, p.value.p2.y);
+            pd.close();
+            Paint pdPaint = Paint();
+            canvas.drawPath(
+                pd,
+                pdPaint
+                  ..shader = ui.Gradient.linear(
                     Offset(p.value.a.x, p.value.a.y),
                     Offset(p.value.p2.x, p.value.p2.y),
-                    [Colors.black12, Colors.transparent]));
+                    [Colors.black12, Colors.transparent],
+                  )
+                  ..style = PaintingStyle.fill);
+            Paint psPaint = Paint();
+            Path ps = Path();
+            ps.moveTo(p.value.a.x, p.value.a.y);
+            ps.lineTo(twoPoint.x, twoPoint.y);
+            ps.lineTo(p.value.p.x, p.value.p.y);
+            ps.close();
 
-          // 右下
-          Path pr = Path();
-          pr.moveTo(p.value.c.x, p.value.c.y);
-          pr.lineTo(p.value.j.x, p.value.j.y);
-          pr.lineTo(p.value.h.x, p.value.h.y);
-          pr.lineTo(p.value.e.x, p.value.e.y);
-          pr.close();
+            canvas.drawPath(
+                ps,
+                psPaint
+                  ..shader = ui.Gradient.linear(
+                      Offset(p.value.a.x, p.value.a.y),
+                      Offset(p.value.p.x, p.value.p.y),
+                      [Colors.black12, Colors.transparent])
+                  ..style = PaintingStyle.fill);
+            // canvas.drawPath(
+            //     Path.combine(PathOperation.reverseDifference, mPathAB, pyy2),
+            //     paint
+            //       ..color = Colors.green
+            //       ..style = PaintingStyle.stroke);
+            // //
+            // canvas.drawPath(
+            //     Path.combine(PathOperation.reverseDifference, mPathAB, pyy1),
+            //     paint
+            //       ..color = Colors.red
+            //       ..style = PaintingStyle.stroke);
 
-          Path p1 = Path.combine(PathOperation.intersect, pr, mPathAB);
-          Path p2 = Path.combine(PathOperation.difference, p1, mPathB);
+            // canvas.drawPath(
+            //     pyy2,
+            //     paint
+            //       ..color = Colors.red
+            //       ..style = PaintingStyle.fill);
+            // canvas.drawPath(
+            //     mPathAB,
+            //     paint
+            //       ..color = Colors.black
+            //       ..style = PaintingStyle.stroke);
 
-          Offset u = Offset(
-              PaperPoint.toTwoPoint(p.value.a, p.value.f, p.value.d, p.value.i)
-                  .x,
-              PaperPoint.toTwoPoint(p.value.a, p.value.f, p.value.d, p.value.i)
-                  .y);
-          canvas.drawPath(
-              p2,
-              paint
-                ..style = PaintingStyle.fill
-                ..shader = ui.Gradient.linear(
-                    u,
-                    Offset(p.value.g.x, p.value.g.y),
-                    [Colors.black26, Colors.transparent]));
+            Path startYY =
+                Path.combine(PathOperation.reverseDifference, mPathAB, pyy1);
+            Path endYY =
+                Path.combine(PathOperation.reverseDifference, mPathAB, pyy2);
 
-          // canvas.drawLine(o1, o2, paint);
+            canvas.drawPath(
+                mPathB, paint..color = bColor ?? Colors.grey.shade400);
+
+            Paint paint2 = Paint();
+            // 上左
+
+            canvas.drawPath(
+                startYY,
+                paint2
+                  ..style = PaintingStyle.fill
+                  ..shader = ui.Gradient.linear(
+                      Offset(p.value.a.x, p.value.a.y),
+                      Offset(p.value.p.x, p.value.p.y),
+                      [Colors.black12, Colors.transparent]));
+
+            //上右
+            canvas.drawPath(
+                endYY,
+                paint2
+                  ..style = PaintingStyle.fill
+                  ..shader = ui.Gradient.linear(
+                      Offset(p.value.a.x, p.value.a.y),
+                      Offset(p.value.p2.x, p.value.p2.y),
+                      [Colors.black12, Colors.transparent]));
+
+            // 右下
+            Path pr = Path();
+            pr.moveTo(p.value.c.x, p.value.c.y);
+            pr.lineTo(p.value.j.x, p.value.j.y);
+            pr.lineTo(p.value.h.x, p.value.h.y);
+            pr.lineTo(p.value.e.x, p.value.e.y);
+            pr.close();
+
+            Path p1 = Path.combine(PathOperation.intersect, pr, mPathAB);
+            Path p2 = Path.combine(PathOperation.difference, p1, mPathB);
+
+            Offset u = Offset(
+                PaperPoint.toTwoPoint(
+                        p.value.a, p.value.f, p.value.d, p.value.i)
+                    .x,
+                PaperPoint.toTwoPoint(
+                        p.value.a, p.value.f, p.value.d, p.value.i)
+                    .y);
+            canvas.drawPath(
+                p2,
+                paint
+                  ..style = PaintingStyle.fill
+                  ..shader = ui.Gradient.linear(
+                      u,
+                      Offset(p.value.g.x, p.value.g.y),
+                      [Colors.black26, Colors.transparent]));
+
+            // canvas.drawLine(o1, o2, paint);
+          }
         }
+      } else {
+        // canvas.drawPath(mPath, paint..color = Colors.red);
       }
-    } else {
-      // canvas.drawPath(mPath, paint..color = Colors.red);
-    }
+    } catch (e) {}
   }
 
   @override
